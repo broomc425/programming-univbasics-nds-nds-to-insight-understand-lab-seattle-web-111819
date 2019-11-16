c2ccb057a18511b46170ada364dd939a52886288
Charlie Broom <broomc425@gmail.com>
2019-11-16 21:11:15 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,8 +12,5 @@ def print_first_directors_movie_titles
   index = 0
   
   while index < ss_movies.length do
-    titles = ss_movies[index][:title]
-    puts titles
-    index += 1
-  end
+    director
 end
